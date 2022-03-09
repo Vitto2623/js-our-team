615e96fdc690f770590b9597b8a6ac6501bcf961
Vitto2623 <vtr.1@icloud.com>
2022-03-09 15:10:06 +0100

    inizializzo index.html

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+//MILESTONE 1: stampare su console le informazioni di nome, ruolo e la stringa della foto
+console.log(team);
+//
+console.log(team[0]['name']);
+console.log(team[1]['name']);
+console.log(team[2]['name']);
+console.log(team[3]['name']);
+console.log(team[4]['name']);
+console.log(team[5]['name']);
+//
+console.log(team[0]['role']);
+console.log(team[1]['role']);
+console.log(team[2]['role']);
+console.log(team[3]['role']);
+console.log(team[4]['role']);
+console.log(team[5]['role']);
+//
+console.log(team[0]['image']);
+console.log(team[1]['image']);
+console.log(team[2]['image']);
+console.log(team[3]['image']);
+console.log(team[4]['image']);
+console.log(team[5]['image']);
